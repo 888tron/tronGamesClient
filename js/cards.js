306c59229bb147f4e0627d77d80df9b1e6dfdb4e
pcherepenchuk <pavlo.cherepenchuk@gmail.com>
2019-01-16 21:38:48 +0200

    update cards

--- js/cards.js
+++ js/cards.js
@@ -253,6 +253,6 @@ function cardType(card) {
             value = 'A';
             break;
     }
-    return '<svg class="icon white"><use xlink:href="img/cards/cards-sprite.svg#' + sign + '"></use></svg><span>' + value + '</span>';
+    return '<span class="mr-1"><svg class="icon white"><use xlink:href="img/cards/cards-sprite.svg#' + sign + '"></use></svg></span><span>' + value + '</span>';
 }
 
